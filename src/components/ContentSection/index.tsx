bc71a34e40a80f9f40ae898bd01a67e09e852bb3
ezg27 <georgehams27@gmail.com>
2020-05-07 16:41:22 +0100

    Move setActiveTab down to TabList component

--- src/components/ContentSection/index.tsx
+++ src/components/ContentSection/index.tsx
@@ -6,16 +6,11 @@ type ContentSectionProps = {
   currentTabs: chrome.tabs.Tab[];
 };
 
-const setActiveTab = (tab: chrome.tabs.Tab) => {
-  if (tab.active || !tab.id) return;
-  chrome.tabs.update(tab.id, { active: true });
-};
-
 const ContentSection: React.FC<ContentSectionProps> = props => {
   return (
     <main className={styles.contentSection}>
       <h3>Current window</h3>
-      <TabList setActiveTab={setActiveTab} {...props} />
+      <TabList {...props} />
     </main>
   );
 };
--- src/components/TabList/index.tsx
+++ src/components/TabList/index.tsx
@@ -3,10 +3,14 @@ import styles from './TabList.module.scss';
 
 type TabListProps = {
   currentTabs: chrome.tabs.Tab[];
-  setActiveTab: (tab: chrome.tabs.Tab) => void;
 };
 
-const TabList: React.FC<TabListProps> = ({ currentTabs, setActiveTab }) => {
+const setActiveTab = (tab: chrome.tabs.Tab) => {
+  if (tab.active || !tab.id) return;
+  chrome.tabs.update(tab.id, { active: true });
+};
+
+const TabList: React.FC<TabListProps> = ({ currentTabs }) => {
   return (
     <div className={styles.tabList}>
       <ul>
