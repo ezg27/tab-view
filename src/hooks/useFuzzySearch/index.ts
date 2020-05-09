826f1cb5403f43a2ef30dda4fa696966af6ff9dd
ezg27 <georgehams27@gmail.com>
2020-05-09 18:00:13 +0100

    Remove console.log from hook

--- src/hooks/useFuzzySearch/index.ts
+++ src/hooks/useFuzzySearch/index.ts
@@ -4,7 +4,6 @@ import { useMemo } from 'react';
 export function useFuzzySearch<T>(keyword: string, data: T[], options: Fuse.IFuseOptions<T>): T[] {
   if (!keyword) return data;
   const searcher = useMemo(() => {
-    console.log('lsakdjglkasjdglkjasdg');
     return new Fuse(data, options);
   }, [data, options]);
   return searcher.search(keyword).map(tab => tab.item);
