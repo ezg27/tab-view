a4209edb98b6dd89567b818f937feeea505bb499
ezg27 <georgehams27@gmail.com>
2020-05-13 22:27:56 +0100

    Extra logic from ContentSection into App component

--- src/components/ContentSection/index.tsx
+++ src/components/ContentSection/index.tsx
@@ -1,42 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import TabList from '../TabList';
+import React from 'react';
 import styles from './ContentSection.module.scss';
 
-type ContentSectionProps = {
-  searchTerm: string;
-};
+type ContentSectionProps = {};
 
-const ContentSection: React.FC<ContentSectionProps> = props => {
-  // TODO: create lazy initial state function to get all windows/tabs instead of useEffect
-  // OR: create custom hook that returns current window and other windows
-  const [currentWindow, setCurrentWindow] = useState<chrome.windows.Window>({} as chrome.windows.Window);
-  const [otherWindows, setOtherWindows] = useState<chrome.windows.Window[]>([]);
-
-  // Get all tabs for all windows
-  useEffect(() => {
-    chrome.windows.getCurrent({ populate: true }, window => {
-      setCurrentWindow(window);
-      chrome.windows.getAll({ populate: true }, allWindows => {
-        const otherWindows = allWindows.filter(windowItem => windowItem.id !== window.id);
-        setOtherWindows(otherWindows);
-      });
-    });
-  }, []);
-
-  return (
-    <main className={styles.contentSection}>
-      <h3>Current window</h3>
-      <TabList window={currentWindow} {...props} />
-      {!!otherWindows.length && (
-        <>
-          <h3>Other windows</h3>
-          {otherWindows.map(window => (
-            <TabList key={window.id} window={window} {...props} />
-          ))}
-        </>
-      )}
-    </main>
-  );
+const ContentSection: React.FC<ContentSectionProps> = ({ children }) => {
+  return <main className={styles.contentSection}>{children}</main>;
 };
 
 export default ContentSection;
--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ContentSection from '../../components/ContentSection';
 import Header from '../../components/Header';
+import TabList from '../../components/TabList';
 import styles from './App.module.scss';
 
 const App: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  // TODO: create lazy initial state function to get all windows/tabs instead of useEffect
+  // OR: create custom hook that returns current window and other windows
+  const [currentWindow, setCurrentWindow] = useState<chrome.windows.Window>({} as chrome.windows.Window);
+  const [otherWindows, setOtherWindows] = useState<chrome.windows.Window[]>([]);
+
+  // Get all tabs for all windows
+  useEffect(() => {
+    chrome.windows.getCurrent({ populate: true }, window => {
+      setCurrentWindow(window);
+      chrome.windows.getAll({ populate: true }, allWindows => {
+        const otherWindows = allWindows.filter(windowItem => windowItem.id !== window.id);
+        setOtherWindows(otherWindows);
+      });
+    });
+  }, []);
 
   return (
     <div className={styles.app}>
       <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <ContentSection searchTerm={searchTerm} />
+      <ContentSection>
+        <h3>Current window</h3>
+        <TabList searchTerm={searchTerm} window={currentWindow} />
+        {!!otherWindows.length && (
+          <>
+            <h3>Other windows</h3>
+            {otherWindows.map(window => (
+              <TabList key={window.id} searchTerm={searchTerm} window={window} />
+            ))}
+          </>
+        )}
+      </ContentSection>
     </div>
   );
 };
