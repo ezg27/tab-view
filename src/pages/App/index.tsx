394ae67bc740699550f457168c88eb14bb42403f
ezg27 <georgehams27@gmail.com>
2020-05-21 20:30:45 +0100

    Refresh current window tab list when tab closed

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,41 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import chromep from 'chrome-promise';
+import React, { useCallback, useEffect, useState } from 'react';
 import ContentSection from '../../components/ContentSection';
 import Header from '../../components/Header';
 import TabList from '../../components/TabList';
 import styles from './App.module.scss';
 
-const setActiveTab = (tab: chrome.tabs.Tab, parentWindow: chrome.windows.Window): void => {
-  chrome.windows.getCurrent(currentWindow => {
-    if (!tab.id) return;
-    if (currentWindow.id !== parentWindow.id) {
-      chrome.windows.update(parentWindow.id, { focused: true }, () => window.close());
-    }
-    if (tab.active) return;
-    chrome.tabs.update(tab.id, { active: true });
-  });
-};
-
-const closeTab = (tab: chrome.tabs.Tab): void => {
-  if (!tab.id) return;
-  chrome.tabs.remove(tab.id);
-};
+interface ChromeWindow extends chrome.windows.Window {
+  isActiveWindow: boolean;
+}
 
 const App: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  // TODO: create lazy initial state function to get all windows/tabs instead of useEffect
-  // OR: create custom hook that returns current window and other windows
-  const [currentWindow, setCurrentWindow] = useState<chrome.windows.Window>({} as chrome.windows.Window);
+  const [currentWindow, setCurrentWindow] = useState<ChromeWindow>({} as ChromeWindow);
   const [otherWindows, setOtherWindows] = useState<chrome.windows.Window[]>([]);
+
+  // Refresh window
+  useEffect(() => {
+    const refreshWindow = (tabId, removeInfo) => {
+      chrome.windows.getCurrent({ populate: true }, window => {
+        setCurrentWindow(window);
+      });
+    };
+    chrome.tabs.onRemoved.addListener(refreshWindow);
+    return () => {
+      chrome.tabs.onRemoved.removeListener(refreshWindow);
+    };
+  }, []);
 
   // Get all tabs for all windows
   useEffect(() => {
-    chrome.windows.getCurrent({ populate: true }, window => {
-      setCurrentWindow(window);
-      chrome.windows.getAll({ populate: true }, allWindows => {
-        const otherWindows = allWindows.filter(windowItem => windowItem.id !== window.id);
-        setOtherWindows(otherWindows);
-      });
-    });
+    const getWindows = async () => {
+      const currentWindow = await chromep.windows.getCurrent({ populate: true });
+      const activeWindow: ChromeWindow = { ...currentWindow, isActiveWindow: true };
+      setCurrentWindow(activeWindow);
+
+      const otherWindows = (await chromep.windows.getAll({ populate: true }))
+        .filter(windowItem => windowItem.id !== currentWindow.id)
+        .map(window => ({ ...window, isActiveWindow: false }));
+      setOtherWindows(otherWindows);
+    };
+    getWindows();
+  }, []);
+
+  const setActiveTab = useCallback(async (tab: chrome.tabs.Tab, parentWindow: chrome.windows.Window): Promise<void> => {
+    if (!tab.id) return;
+    const currentWindow = await chromep.windows.getCurrent();
+    if (currentWindow.id !== parentWindow.id) {
+      await chromep.windows.update(parentWindow.id, { focused: true }, () => window.close());
+    }
+    if (tab.active) return;
+    chromep.tabs.update(tab.id, { active: true });
+  }, []);
+
+  const closeTab = useCallback(async (tab: chrome.tabs.Tab): Promise<void> => {
+    if (!tab.id) return;
+    await chromep.tabs.remove(tab.id);
   }, []);
 
   return (
