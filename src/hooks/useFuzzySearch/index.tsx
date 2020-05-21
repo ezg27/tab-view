cb1f107912bba801ddc982ab357d3e464e568482
ezg27 <georgehams27@gmail.com>
2020-05-21 17:16:37 +0100

    Remove incorrect use of useMemo inside conditional

--- src/hooks/useFuzzySearch/index.tsx
+++ src/hooks/useFuzzySearch/index.tsx
@@ -1,10 +1,7 @@
 import Fuse from 'fuse.js';
-import { useMemo } from 'react';
 
 export function useFuzzySearch<T>(keyword: string, data: T[], options: Fuse.IFuseOptions<T>): T[] {
   if (!keyword) return data;
-  const searcher = useMemo(() => {
-    return new Fuse(data, options);
-  }, [data, options]);
+  const searcher = new Fuse(data, options);
   return searcher.search(keyword).map(tab => tab.item);
 }
