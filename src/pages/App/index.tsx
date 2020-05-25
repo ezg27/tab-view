898781e15746797d78b485b04ad4141c618e5a2b
ezg27 <georgehams27@gmail.com>
2020-05-25 11:51:16 +0100

    Use listener to refresh window on tab close

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -12,9 +12,9 @@ interface ChromeWindow extends chrome.windows.Window {
 const App: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [currentWindow, setCurrentWindow] = useState<ChromeWindow>({} as ChromeWindow);
-  const [otherWindows, setOtherWindows] = useState<chrome.windows.Window[]>([]);
+  const [otherWindows, setOtherWindows] = useState<ChromeWindow[]>([]);
 
-  // Get all tabs for all windows
+  // Get tabs for all windows
   useEffect(() => {
     const getWindows = async () => {
       const currentWindow = await chromep.windows.getCurrent({ populate: true });
@@ -23,36 +23,68 @@ const App: React.FC = () => {
 
       const otherWindows = (await chromep.windows.getAll({ populate: true }))
         .filter(windowItem => windowItem.id !== currentWindow.id)
-        .map(window => ({ ...window, isActiveWindow: false }));
+        .map(window => ({ ...window, isActiveWindow: false } as ChromeWindow));
       setOtherWindows(otherWindows);
     };
     getWindows();
   }, []);
 
-  // Refresh window
+  // On remove listener
   useEffect(() => {
     const refreshWindow = async (tabId, removeInfo) => {
-      const window = await chromep.windows.getCurrent({ populate: true });
-      setCurrentWindow(window);
+      // Get window tab was removed from
+      const refreshedWindow = await chromep.windows.get(removeInfo.windowId, { populate: true });
+
+      // If active window
+      if (refreshedWindow.id === currentWindow.id) {
+        setCurrentWindow({ ...refreshedWindow, isActiveWindow: true } as ChromeWindow);
+        return;
+      }
+
+      // If tab closed was last in the window
+      if (!refreshedWindow.tabs.length) {
+        setOtherWindows(windows => windows.filter(window => window.id !== refreshedWindow.id));
+        return;
+      }
+
+      setOtherWindows(windows =>
+        windows.map(window => {
+          return window.id === refreshedWindow.id
+            ? ({ ...refreshedWindow, isActiveWindow: false } as ChromeWindow)
+            : window;
+        })
+      );
     };
+
     chrome.tabs.onRemoved.addListener(refreshWindow);
     return () => {
       chrome.tabs.onRemoved.removeListener(refreshWindow);
     };
-  }, []);
+  }, [currentWindow]);
 
-  const setActiveTab = async (tab: chrome.tabs.Tab, parentWindow: chrome.windows.Window): Promise<void> => {
+  const setActiveTab = useCallback(async (tab: chrome.tabs.Tab, parentWindow: chrome.windows.Window): Promise<void> => {
+    // Return if window is invalid
     if (!tab.id) return;
-    if (currentWindow.id !== parentWindow.id) {
+
+    // If tab is in another window, navigate and close extension
+    const current = await chromep.windows.getCurrent();
+    if (current.id !== parentWindow.id) {
       await chromep.windows.update(parentWindow.id, { focused: true });
       window.close();
     }
+
+    // Return if current tab selected
     if (tab.active) return;
+
+    // Navigate to tab in current window
     await chromep.tabs.update(tab.id, { active: true });
-  };
+  }, []);
 
   const closeTab = useCallback(async (tab: chrome.tabs.Tab): Promise<void> => {
+    // Return if window is invalid
     if (!tab.id) return;
+
+    // Remove tab
     await chromep.tabs.remove(tab.id);
   }, []);
 
