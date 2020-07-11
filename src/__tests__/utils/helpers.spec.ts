12430a45b723213409460c7ae1e337609d8f80f7
ezg27 <georgehams27@gmail.com>
2020-07-11 11:58:03 +0100

    Add test for getWIndows

--- src/__tests__/utils/helpers.spec.ts
+++ src/__tests__/utils/helpers.spec.ts
@@ -1,13 +1,38 @@
-import { DraggableLocation } from "react-beautiful-dnd";
-import { mockWindow1, mockWindow2 } from "./../testData/mockWindows";
-import {
-  groupBy,
-  optimisticReorderTabs,
-  optimisticMoveTabBetweenWindows,
-} from "../../utils/helpers";
+import { chrome } from 'jest-chrome';
+import { DraggableLocation } from 'react-beautiful-dnd';
+import { getWindows, groupBy, optimisticMoveTabBetweenWindows, optimisticReorderTabs } from '../../utils/helpers';
+import { mockWindow1, mockWindow2 } from './../testData/mockWindows';
 
-describe("groupBy - Divide passed array into groups according to passed callback", () => {
-  test("Returns empty arrays when passed empty list", () => {
+describe('getWindows', () => {
+  test('should return window list with active window assigned', async () => {
+    // Arrange
+
+    // Pass mockWindow2 as the active window
+    chrome.windows.getCurrent.mockImplementation(
+      // @ts-ignore - Typescript not picking up callback overload that accepts
+      // just callback, defaults instead to signature (getInfo: chrome.windows.GetInfo, callback...) etc.
+      (callback: (window: chrome.windows.Window) => void) => callback({ ...mockWindow2 })
+    );
+
+    // Mock getAll windows
+    chrome.windows.getAll.mockImplementation(
+      (getInfo: chrome.windows.GetInfo, callback: (window: chrome.windows.Window[]) => void) =>
+        callback([{ ...mockWindow1 }, { ...mockWindow2 }])
+    );
+
+    // Act
+    const result = await getWindows();
+
+    // Assert
+    expect(result).toEqual([
+      { ...mockWindow1, isActiveWindow: false },
+      { ...mockWindow2, isActiveWindow: true },
+    ]);
+  });
+});
+
+describe('groupBy - Divide passed array into groups according to passed callback', () => {
+  test('Returns empty arrays when passed empty list', () => {
     // Arrange
     const list: any[] = [];
     const callback = (item: string) => item.length > 5;
@@ -20,7 +45,7 @@ describe("groupBy - Divide passed array into groups according to passed callback
     expect(result).toEqual(expected);
   });
 
-  test("Returns correctly grouped values", () => {
+  test('Returns correctly grouped values', () => {
     // Arrange
     const list = [1, 9, 4, 7, 8, 3, 5];
     const callback = (item: number) => item >= 5;
@@ -37,20 +62,18 @@ describe("groupBy - Divide passed array into groups according to passed callback
   });
 });
 
-describe("optimisticReorderTabs - reorder tabs within a single window", () => {
-  test("throws error if window not present in list", () => {
+describe('optimisticReorderTabs - reorder tabs within a single window', () => {
+  test('throws error if window not present in list', () => {
     const windows = [{ ...mockWindow1 }];
-    const destination = { index: -1, droppableId: "888" } as DraggableLocation;
-    const source = { index: -1, droppableId: "999" } as DraggableLocation;
+    const destination = { index: -1, droppableId: '888' } as DraggableLocation;
+    const source = { index: -1, droppableId: '999' } as DraggableLocation;
 
-    expect(() =>
-      optimisticReorderTabs(windows, destination, source)
-    ).toThrowErrorMatchingInlineSnapshot(
+    expect(() => optimisticReorderTabs(windows, destination, source)).toThrowErrorMatchingInlineSnapshot(
       `"Window could not be found for optimistic reorder."`
     );
   });
 
-  test("should correctly reorder tabs", () => {
+  test('should correctly reorder tabs', () => {
     const windows = [{ ...mockWindow1 }];
     const destination = {
       index: 1,
@@ -61,7 +84,7 @@ describe("optimisticReorderTabs - reorder tabs within a single window", () => {
       droppableId: `${mockWindow1.id}`,
     } as DraggableLocation;
 
-    const intialTabs = windows[0].tabs!.map((tab) => tab.title);
+    const intialTabs = windows[0].tabs!.map(tab => tab.title);
     expect(intialTabs).toMatchInlineSnapshot(`
       Array [
         "Test tab 1",
@@ -70,7 +93,7 @@ describe("optimisticReorderTabs - reorder tabs within a single window", () => {
     `);
 
     const resultWindow = optimisticReorderTabs(windows, destination, source);
-    const reorderedTabs = resultWindow![0].tabs!.map((tab) => tab.title);
+    const reorderedTabs = resultWindow![0].tabs!.map(tab => tab.title);
     expect(reorderedTabs).toMatchInlineSnapshot(`
       Array [
         "Test tab 2",
@@ -80,20 +103,18 @@ describe("optimisticReorderTabs - reorder tabs within a single window", () => {
   });
 });
 
-describe("optimisticMoveTabBetweenWindows - move tabs between windows", () => {
-  test("throws error if windows not present in list", () => {
+describe('optimisticMoveTabBetweenWindows - move tabs between windows', () => {
+  test('throws error if windows not present in list', () => {
     const windows = [{ ...mockWindow1 }, { ...mockWindow2 }];
-    const destination = { index: -1, droppableId: "888" } as DraggableLocation;
-    const source = { index: -1, droppableId: "999" } as DraggableLocation;
+    const destination = { index: -1, droppableId: '888' } as DraggableLocation;
+    const source = { index: -1, droppableId: '999' } as DraggableLocation;
 
-    expect(() =>
-      optimisticMoveTabBetweenWindows(windows, destination, source)
-    ).toThrowErrorMatchingInlineSnapshot(
+    expect(() => optimisticMoveTabBetweenWindows(windows, destination, source)).toThrowErrorMatchingInlineSnapshot(
       `"Source or destination windows could not be found for optimsitic move between windows."`
     );
   });
 
-  test("should correctly move tabs", () => {
+  test('should correctly move tabs', () => {
     const windows = [{ ...mockWindow1 }, { ...mockWindow2 }];
     const destination = {
       index: 1,
@@ -104,9 +125,7 @@ describe("optimisticMoveTabBetweenWindows - move tabs between windows", () => {
       droppableId: `${mockWindow1.id}`,
     } as DraggableLocation;
 
-    const intialTabs = windows.map((window) =>
-      window.tabs!.map((tab) => tab.title)
-    );
+    const intialTabs = windows.map(window => window.tabs!.map(tab => tab.title));
     expect(intialTabs).toMatchInlineSnapshot(`
       Array [
         Array [
@@ -120,14 +139,8 @@ describe("optimisticMoveTabBetweenWindows - move tabs between windows", () => {
       ]
     `);
 
-    const result = optimisticMoveTabBetweenWindows(
-      windows,
-      destination,
-      source
-    );
-    const reorderedTabs = result.map((window) =>
-      window.tabs!.map((tab) => tab.title)
-    );
+    const result = optimisticMoveTabBetweenWindows(windows, destination, source);
+    const reorderedTabs = result.map(window => window.tabs!.map(tab => tab.title));
     expect(reorderedTabs).toMatchInlineSnapshot(`
       Array [
         Array [
